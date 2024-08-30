1239a5798f1b1695d3a6775f346ccb095e32e2de
Patryk <patryk.rakowski@cwmlegal.pl>
2024-08-30 13:45:13 +0200

    first-app 01 4 strona

--- first-app_01-hello-world/src/app/home/home.component.ts
+++ first-app_01-hello-world/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
 import { CommonModule } from "@angular/common";
+import { HousingLocation} from "../housinglocation";
 
 @Component({
   selector: 'app-home',
@@ -20,5 +21,16 @@ import { CommonModule } from "@angular/common";
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
