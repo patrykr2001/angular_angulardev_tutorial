243e917a2225b55a7eb7c4bf5278f74b454eb1e6
Patryk <patryk.rakowski@cwmlegal.pl>
2024-08-30 13:57:32 +0200

    first-app 01 5 strona

--- first-app_01-hello-world/src/app/housing-location/housing-location.component.ts
+++ first-app_01-hello-world/src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {HousingLocation} from "../housinglocation";
 
 @Component({
   selector: 'app-housing-location',
@@ -12,5 +14,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.component.scss'],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
